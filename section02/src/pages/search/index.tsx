a10524b78dc5205727b81b39acfc01ee17d42d21
goosebbeoms <seo980916@gmail.com>
2024-10-12 21:31:44 +0900

    section02 / 2-9) 한입 북스(Onebite Books) UI 구현

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,18 +1,19 @@
 import SearchableLayout from "@/components/searchable-layout";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
 export default function Page() {
-  const router = useRouter(); // router 객체가 저장됨
-  // console.log(router);
-
-  // const q = router.query.q;
-  const { q } = router.query;
-  console.log(q);
-
-  return <h1>Search {q}</h1>;
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
-  return <SearchableLayout>{page}</SearchableLayout>
-}
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
