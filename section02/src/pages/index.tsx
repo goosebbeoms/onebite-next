be8ff6e4353178429f200ab049d1b872b0ae454d
goosebbeoms <seo980916@gmail.com>
2024-10-21 23:00:23 +0900

    2-14) SSG 2. 정적 경로에 적용하기

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -2,13 +2,12 @@ import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
 import { ReactNode } from "react";
 import BookItem from "@/components/book-item";
-import { InferGetServerSidePropsType } from "next";
+import { InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export const getServerSideProps = async () => {
-  // const allBooks = await fetchBooks();
-  // const recoBooks = await fetchRandomBooks();
+export const getStaticProps = async () => {
+  console.log("인덱스 페이지");
 
   const [allBooks, recoBooks] = await Promise.all([fetchBooks(), fetchRandomBooks()]);
   return {
@@ -19,7 +18,7 @@ export const getServerSideProps = async () => {
   };
 };
 
-export default function Home({ allBooks, recoBooks }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Home({ allBooks, recoBooks }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={style.container}>
       <section>
